b669f1fe04d6085ad1b558881ccc44cbb3b06d6d
lordst95 <sina.tamjidy@gmail.com>
2017-01-25 13:26:12 -0500

    Improving notification part ;)

--- SAD_Project_MainSite/showSomeResult/urls.py
+++ SAD_Project_MainSite/showSomeResult/urls.py
@@ -6,9 +6,12 @@ urlpatterns = [
     url(r'^$', views.guide, name='guide'),                                  # http://127.0.0.1:8000/searchSTH/
     url(r'^topGenre/$', views.showTopGenre, name='showTopGenre'),           # http://127.0.0.1:8000/searchSTH/topGenre/
     url(r'^tarjomeList/$', views.TarjomeList, name='TarjomeList'),  # http://127.0.0.1:8000/searchSTH/tarjomeList/
+
     # http://127.0.0.1:8000/searchSTH/takhfif/2/
     url(r'^takhfif/(?P<buyedItems>[0-9]+)/$', views.gettingNewBuyRequest, name='gettingNewBuyRequest'),
+
     # http://127.0.0.1:8000/searchSTH/update/2/
     url(r'^update/(?P<book_id>[0-9]+)/$', views.getNewTarjomeReq, name='getNewTarjomeReq'),
+
     url(r'^bootiTest/$', views.bootiTest, name='bootiTest'),  # http://127.0.0.1:8000/searchSTH/bootiTest/
 ]
